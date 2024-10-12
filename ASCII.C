b2825c3cf838cf431178060215e7788dcf70a6a8
ankush6263 <127200265+ankush6263@users.noreply.github.com>
2024-10-12 14:09:36 +0530

    Create ASCII.C

---
+++ ASCII.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char c;
+    printf("enter value of character:\n");
+    scanf("%c",&c);
+    printf("aissc value of enterd char is :%d", c);
+
+    return 0;
+}
+// YOU CAN FIND AISSC VALUE OF ENTERED CHAR
